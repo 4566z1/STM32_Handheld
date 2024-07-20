9ad5adc3c1405e782bf933e6eb294abd11e542d2
4566z1 <2300115453@qq.com>
2024-07-20 20:57:51 +0800

    使得RFID通过按键进行读取, 优化部分结构和代码

--- UserApp/main.cpp
+++ UserApp/main.cpp
@@ -1,39 +1,21 @@
-#include "bluetooth.h"
-#include "common_inc.h"
-#include "i2c.h"
-#include "rfid.h"
-#include "ssd1306.h"
-
-extern "C" {
-#include "bsp_USART.h"
-}
+#include "main.h"
 
 Rfid rfid;
 BLE ble;
+
+// 出库入库模式
+volatile int mode = 0;
 
 int stm32_main(void)
 {
     ssd1306_Init(&hi2c1);
 
     while (1) {
-        // RFID
-        int mode = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_6);
-        {
-            if (rfid.read()) {
-                LOG("stm32_main => name: %s code: %s mode: %d \r\n", rfid.get_name(), rfid.get_code(), mode);
-
-                ble.send(rfid.get_name(), rfid.get_code(), mode);
-
-                HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
-                HAL_Delay(100);
-                HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
-            }
-        }
-
-        // SCREEN
+        mode = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_6);
         {
             char str[20] = {0};
 
+            // 显示出库入库模式
             !mode ? sprintf(str, "ADD MODE") : sprintf(str, "DEL MODE");
             ssd1306_SetCursor(0, 32);
             ssd1306_WriteString(str, Font_16x26, White);
@@ -46,12 +28,33 @@ int stm32_main(void)
 }
 
 /**
+ * @brief RFID 回调函数
+ *
+ */
+void rfid_handler()
+{
+    if (rfid.read()) {
+        // LOG("stm32_main => name: %s code: %s mode: %d \r\n", rfid.get_name(), rfid.get_code(), mode);
+
+        // 蓝牙发送
+        ble.send(rfid.get_name(), rfid.get_code(), (const int&)mode);
+
+        // 蜂鸣器
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+        HAL_Delay(100); 
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
+    }
+}
+
+/**
  * @brief 按键中断
- * 
+ *
  */
 extern "C" void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 {
-    if (GPIO_Pin == GPIO_PIN_4) {
+    if (GPIO_Pin == GPIO_PIN_1) {
+        rfid_handler();
+    } else if (GPIO_Pin == GPIO_PIN_4) {
         HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_6);
     }
 }
---
+++ UserApp/main.h
@@ -0,0 +1,14 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include "bluetooth.h"
+#include "common_inc.h"
+#include "i2c.h"
+#include "rfid.h"
+#include "ssd1306.h"
+
+extern "C" {
+#include "bsp_USART.h"
+}
+
+#endif
